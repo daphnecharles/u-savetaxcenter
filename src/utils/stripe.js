c34b00779dc60e396d10fb5efe8b781a8a878495
Daphne-Charles <daphneacharles@gmail.com>
2020-12-13 18:29:02 -0500

    test

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -6,10 +6,13 @@ import { loadStripe } from "@stripe/stripe-js";
 let stripePromise;
 const getStripe = () => {
   if (!stripePromise) {
-    stripePromise = loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
+    stripePromise = loadStripe(`${process.env.GATSBY_STRIPE_PUBLISHABLE_KEY}`);
   }
-  console.log("publishable key is: ", process.env.STRIPE_PUBLISHABLE_KEY);
-  console.log("secret key is: ", process.env.STRIPE_SECRET_KEY);
+  console.log(
+    "publishable key is: ",
+    process.env.GATSBY_STRIPE_PUBLISHABLE_KEY
+  );
+  console.log("secret key is: ", process.env.GATSBY_STRIPE_SECRET_KEY);
   return stripePromise;
 };
 
