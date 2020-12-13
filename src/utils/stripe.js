b27934374acb2aa65c2726d068df4190a2a519b9
Daphne-Charles <daphneacharles@gmail.com>
2020-12-13 18:10:03 -0500

    testing

--- src/utils/stripe.js
+++ src/utils/stripe.js
@@ -8,6 +8,8 @@ const getStripe = () => {
   if (!stripePromise) {
     stripePromise = loadStripe(`${process.env.STRIPE_PUBLISHABLE_KEY}`);
   }
+  console.log("publishable key is: ", process.env.STRIPE_PUBLISHABLE_KEY);
+  console.log("secret key is: ", process.env.STRIPE_SECRET_KEY);
   return stripePromise;
 };
 
